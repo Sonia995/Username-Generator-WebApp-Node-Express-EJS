bcb37413574fa2f0356e14605f83b9c7243da41e
Sonia <amarnas722@gmail.com>
2023-11-09 10:46:37 +0100

    gitignore, procfile, y puerto actualizado

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+let port = process.env.PORT;
+if ( port == null || port == "") {
+  port = 3000;
+}
 
 app.use(express.static("public"));
 
